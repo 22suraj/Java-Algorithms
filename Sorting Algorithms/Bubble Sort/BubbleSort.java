5db5260f5669bfb5ca7588a2bac58248681b0235
22suraj <59668309+22suraj@users.noreply.github.com>
2021-06-13 01:03:31 +0530

    Add files via upload

---
+++ Sorting Algorithms/Bubble Sort/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+
+    static void bubbleSort(int a[]) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j + 1] < a[j]) {
+                    int temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 9, 4, 5, 6, 8, 15, 2 };
+
+        bubbleSort(arr);
+
+    }
+}
